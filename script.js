fd8ec753e16d043e31c7d8166b4fc36f2429f9f3
MAR Pierre <pierre.mar.66000@gmail.com>
2017-11-16 10:57:11 +0100

    Update script for #sections

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-var $ = jQuery;
-$(document).ready(function(){
+// var $ = jQuery;
+// $(document).ready(function(){
 
-    $('.wpcf7-form select:not([name=program])').each(function(){
+//     $('.wpcf7-form select:not([name=program])').each(function(){
 
-        $(this).children("option").each(function( i , $obj ){
+//         $(this).children("option").each(function( i , $obj ){
 
-            $(this).val(i+1);
+//             $(this).val(i+1);
+
+//         });
+
+//     });
+
+// });
+var jQuery = jQuery;
+jQuery(document).ready(function(){
+
+    jQuery('.wpcf7-form select:not([name=program])').each(function(){
+
+        jQuery(this).children("option").each(function( i , jQueryobj ){
+
+            var html = jQuery(this).html();
+            var position = html.search("#");
+
+            if( position > -1 ){
+                var value = html.slice(position+1, html.length);
+                html = html.slice(0, position);
+                jQuery(this).html(html);
+                jQuery(this).val(value);
+            }
+            else
+                jQuery(this).val(i+1);
 
         });
 
